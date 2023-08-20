d4a3f7f024575f03009aa9852ad81f951d4b45b2
Bowen <hwbrzzl@gmail.com>
2023-08-20 17:27:30 +0800

    Add grpc interceptors

--- app/grpc/interceptors/opentracing_server.go
+++ app/grpc/interceptors/opentracing_server.go
@@ -10,27 +10,28 @@ import (
 	"google.golang.org/grpc/metadata"
 )
 
-func OpentracingServer(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (resp interface{}, err error) {
-	tracer, closer := NewJaegerTracer()
-	defer closer.Close()
+func OpentracingServer(tracer opentracing.Tracer) grpc.UnaryServerInterceptor {
+	return func(ctx context.Context, req any, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (resp any, err error) {
+		var parentCtx context.Context
 
-	md, ok := metadata.FromIncomingContext(ctx)
-	if !ok {
-		md = metadata.New(nil)
+		md, ok := metadata.FromIncomingContext(ctx)
+		if !ok {
+			md = metadata.New(nil)
+		}
+		spanContext, err := tracer.Extract(opentracing.TextMap, MDReaderWriter{md})
+		if err != nil {
+			return nil, fmt.Errorf("extract from metadata error: %v", err)
+		} else {
+			span := tracer.StartSpan(
+				info.FullMethod,
+				ext.RPCServerOption(spanContext),
+				opentracing.Tag{Key: string(ext.Component), Value: "gRPC"},
+				ext.SpanKindRPCServer,
+			)
+			defer span.Finish()
+			parentCtx = opentracing.ContextWithSpan(ctx, span)
+		}
+
+		return handler(parentCtx, req)
 	}
-
-	spanContext, err := tracer.Extract(opentracing.TextMap, MDReaderWriter{md})
-	if err != nil && err != opentracing.ErrSpanContextNotFound {
-		return nil, fmt.Errorf("extract from metadata error: %v", err)
-	}
-
-	span := tracer.StartSpan(
-		info.FullMethod,
-		ext.RPCServerOption(spanContext),
-		opentracing.Tag{Key: string(ext.Component), Value: "gRPC"},
-		ext.SpanKindRPCServer,
-	)
-	defer span.Finish()
-
-	return handler(opentracing.ContextWithSpan(ctx, span), req)
 }
