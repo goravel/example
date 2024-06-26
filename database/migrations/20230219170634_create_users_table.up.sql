a9edfdd420c6f8f738a8f2c9c28d424009644dd7
Wenbo Han <hwbrzzl@gmail.com>
2024-06-26 22:59:32 +0800

    chore: add test action (#19)
    * chore: add test action
    
    * change to postgres
    
    * update migration
    
    * update action
    
    * update action
    
    * update action

--- database/migrations/20230219170634_create_users_table.up.sql
+++ database/migrations/20230219170634_create_users_table.up.sql
@@ -1,11 +1,8 @@
 CREATE TABLE users (
-  id bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-  name varchar(191) DEFAULT '' NOT NULL,
-  avatar varchar(191) DEFAULT '' NOT NULL,
-  created_at datetime(3) NOT NULL,
-  updated_at datetime(3) NOT NULL,
-  deleted_at datetime(3) DEFAULT NULL,
-  PRIMARY KEY (id),
-  KEY idx_users_created_at (created_at),
-  KEY idx_users_updated_at (updated_at)
-) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
+  id SERIAL PRIMARY KEY NOT NULL,
+  name varchar(255) DEFAULT '' NOT NULL,
+  avatar varchar(255) DEFAULT '' NOT NULL,
+  created_at timestamp NOT NULL,
+  updated_at timestamp NOT NULL,
+  deleted_at timestamp DEFAULT NULL
+);
