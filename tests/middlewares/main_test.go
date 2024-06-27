9dc49cdc4848aaffb9e4693b2e243af7b196b2c9
耗子 <i@haozi.net>
2024-06-28 00:29:32 +0800

    fix: test

--- tests/middlewares/main_test.go
+++ tests/middlewares/main_test.go
@@ -5,20 +5,11 @@ import (
 	"testing"
 
 	"github.com/goravel/framework/facades"
-	"github.com/goravel/framework/support/file"
 	"github.com/goravel/framework/support/str"
 )
 
 func TestMain(m *testing.M) {
-	go func() {
-		if err := facades.Route().Run(); err != nil {
-			facades.Log().Errorf("Route run error: %v", err)
-		}
-	}()
-
 	m.Run()
-
-	file.Remove("storage")
 }
 
 func route(path string) string {
--- tests/test_case.go
+++ tests/test_case.go
@@ -1,6 +1,7 @@
 package tests
 
 import (
+	"github.com/goravel/framework/facades"
 	"github.com/goravel/framework/testing"
 
 	"goravel/bootstrap"
@@ -8,6 +9,12 @@ import (
 
 func init() {
 	bootstrap.Boot()
+
+	go func() {
+		if err := facades.Route().Run(); err != nil {
+			facades.Log().Errorf("Route run error: %v", err)
+		}
+	}()
 }
 
 type TestCase struct {
