1aee6699a6a03267e132fbfcea76f78145337923
Bowen <hwbrzzl@gmail.com>
2024-09-01 19:09:22 +0800

    remove comment

--- main.go
+++ main.go
@@ -14,27 +14,8 @@ func main() {
 	// This bootstraps the framework and gets it ready for use.
 	bootstrap.Boot()
 
-	// Start HTTP server by facades.Route().
-	//go func() {
-	//	if err := facades.Route().Run(); err != nil {
-	//		facades.Log().Errorf("Route run error: %v", err)
-	//	}
-	//}()
-	//
-	//// Start GRPC server
-	//go func() {
-	//	if err := facades.Grpc().Run(); err != nil {
-	//		facades.Log().Errorf("Run grpc error: %+v", err)
-	//	}
-	//}()
-
-	//select {}
-
 	var restaurant models.Restaurant
 	query := facades.Orm().Query().(*gorm.QueryImpl)
-	//if err := query.Instance().SetupJoinTable(&models.Restaurant{}, "Categories", &models.RestaurantCategory{}); err != nil {
-	//	fmt.Printf("SetupJoinTable error: %+v\n", err)
-	//}
 	if err := query.With("Categories").Find(&restaurant, 1); err != nil {
 		fmt.Printf("Find error: %+v\n", err)
 	}
