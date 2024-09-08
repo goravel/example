f4a8f066201f145c24715f9846b05dd357925361
Bowen <hwbrzzl@gmail.com>
2024-09-08 11:32:26 +0800

    optimize test cases

--- tests/feature/route_test.go
+++ tests/feature/route_test.go
@@ -17,7 +17,7 @@ import (
 type RouteTestSuite struct {
 	suite.Suite
 	tests.TestCase
-	http *resty.Request
+	http *resty.Client
 }
 
 func TestRouteTestSuite(t *testing.T) {
@@ -26,7 +26,7 @@ func TestRouteTestSuite(t *testing.T) {
 			SetBaseURL(fmt.Sprintf("http://%s:%s",
 				facades.Config().GetString("APP_HOST"),
 				facades.Config().GetString("APP_PORT"))).
-			SetHeader("Content-Type", "application/json").R(),
+			SetHeader("Content-Type", "application/json"),
 	})
 }
 
@@ -37,78 +37,6 @@ func (s *RouteTestSuite) SetupTest() {
 
 // TearDownTest will run after each test in the suite.
 func (s *RouteTestSuite) TearDownTest() {
-}
-
-func (s *RouteTestSuite) TestUsers() {
-	// Add a user
-	var createdUser struct {
-		User models.User
-	}
-
-	resp, err := s.http.SetResult(&createdUser).SetBody(map[string]string{
-		"name":   "Goravel",
-		"avatar": "https://goravel.dev/avatar.png",
-	}).Post("users")
-
-	s.Require().NoError(err)
-	s.Require().Equal(http.StatusOK, resp.StatusCode())
-	s.True(createdUser.User.ID > 0)
-	s.Equal("Goravel", createdUser.User.Name)
-	s.Equal("https://goravel.dev/avatar.png", createdUser.User.Avatar)
-
-	// Get Users
-	var users struct {
-		Users []models.User
-	}
-	resp, err = s.http.SetResult(&users).Get("users")
-
-	s.Require().NoError(err)
-	s.Require().Equal(http.StatusOK, resp.StatusCode())
-	s.Equal(1, len(users.Users))
-	s.True(createdUser.User.ID > 0)
-	s.Equal("Goravel", users.Users[0].Name)
-	s.Equal("https://goravel.dev/avatar.png", users.Users[0].Avatar)
-
-	// Update the User
-	var updatedUser struct {
-		User models.User
-	}
-
-	resp, err = s.http.SetResult(&updatedUser).SetBody(map[string]string{
-		"name": "Framework",
-	}).Put(fmt.Sprintf("users/%d", createdUser.User.ID))
-
-	s.Require().NoError(err)
-	s.Require().Equal(http.StatusOK, resp.StatusCode())
-	s.Equal(createdUser.User.ID, updatedUser.User.ID)
-	s.Equal("Framework", updatedUser.User.Name)
-	s.Equal("https://goravel.dev/avatar.png", updatedUser.User.Avatar)
-
-	// Get the User
-	var user struct {
-		User models.User
-	}
-	resp, err = s.http.SetResult(&user).Get(fmt.Sprintf("users/%d", createdUser.User.ID))
-
-	s.Require().NoError(err)
-	s.Require().Equal(http.StatusOK, resp.StatusCode())
-	s.True(user.User.ID > 0)
-	s.Equal("Framework", user.User.Name)
-	s.Equal("https://goravel.dev/avatar.png", user.User.Avatar)
-
-	// Delete the User
-	resp, err = s.http.Delete(fmt.Sprintf("users/%d", createdUser.User.ID))
-
-	s.Require().NoError(err)
-	s.Require().Equal(http.StatusOK, resp.StatusCode())
-	s.Equal("{\"rows_affected\":1}", resp.String())
-
-	// Get Users
-	resp, err = s.http.Get("users")
-
-	s.Require().NoError(err)
-	s.Require().Equal(http.StatusOK, resp.StatusCode())
-	s.Equal("{\"users\":[]}", resp.String())
 }
 
 func (s *RouteTestSuite) TestLang() {
@@ -130,41 +58,13 @@ func (s *RouteTestSuite) TestLang() {
 
 	for _, test := range tests {
 		s.Run(test.name, func() {
-			resp, err := s.http.Get(fmt.Sprintf("/lang?lang=%s", test.lang))
+			resp, err := s.http.R().Get(fmt.Sprintf("/lang?lang=%s", test.lang))
 
 			s.NoError(err)
 			s.Equal(http.StatusOK, resp.StatusCode())
 			s.Equal(test.expectResponse, resp.String())
 		})
 	}
-}
-
-func (s *RouteTestSuite) TestValidationJson() {
-	payload := strings.NewReader(`{
-		"name": "Goravel",
-		"date": "2024-07-08 18:33:32"
-	}`)
-
-	resp, err := s.http.SetBody(payload).Post("/validation/json")
-
-	s.NoError(err)
-	s.Equal(http.StatusOK, resp.StatusCode())
-	s.Equal("{\"date\":\"2024-07-08 18:33:32\",\"name\":\"Goravel\"}", resp.String())
-}
-
-func (s *RouteTestSuite) TestValidationRequest() {
-	payload := strings.NewReader(`{
-		"name": "Goravel",
-		"date": "2024-07-08 18:33:32",
-		"tags": ["tag1", "tag2"],
-		"scores": [1, 2]
-	}`)
-
-	resp, err := s.http.SetBody(payload).Post("/validation/request")
-
-	s.NoError(err)
-	s.Equal(http.StatusOK, resp.StatusCode())
-	s.Equal("{\"date\":\"2024-07-08 18:33:32\",\"name\":\"Goravel\",\"scores\":[1,2],\"tags\":[\"tag1\",\"tag2\"]}", resp.String())
 }
 
 func (s *RouteTestSuite) TestThrottle() {
@@ -187,10 +87,110 @@ func (s *RouteTestSuite) TestThrottle() {
 			var resp *resty.Response
 			var err error
 			for i := 0; i < 5; i++ {
-				resp, err = s.http.Get("/jwt/login")
+				resp, err = s.http.R().Get("/jwt/login")
 				s.Require().NoError(err)
 			}
 			s.Equal(test.expectStatusCode, resp.StatusCode())
 		})
 	}
+}
+
+func (s *RouteTestSuite) TestUsers() {
+	// Add a user
+	var createdUser struct {
+		User models.User
+	}
+
+	resp, err := s.http.R().SetResult(&createdUser).SetBody(map[string]string{
+		"name":   "Goravel",
+		"avatar": "https://goravel.dev/avatar.png",
+	}).Post("users")
+
+	s.Require().NoError(err)
+	s.Require().Equal(http.StatusOK, resp.StatusCode())
+	s.True(createdUser.User.ID > 0)
+	s.Equal("Goravel", createdUser.User.Name)
+	s.Equal("https://goravel.dev/avatar.png", createdUser.User.Avatar)
+
+	// Get Users
+	var users struct {
+		Users []models.User
+	}
+	resp, err = s.http.R().SetResult(&users).Get("users")
+
+	s.Require().NoError(err)
+	s.Require().Equal(http.StatusOK, resp.StatusCode())
+	s.Equal(1, len(users.Users))
+	s.True(createdUser.User.ID > 0)
+	s.Equal("Goravel", users.Users[0].Name)
+	s.Equal("https://goravel.dev/avatar.png", users.Users[0].Avatar)
+
+	// Update the User
+	var updatedUser struct {
+		User models.User
+	}
+
+	resp, err = s.http.R().SetResult(&updatedUser).SetBody(map[string]string{
+		"name": "Framework",
+	}).Put(fmt.Sprintf("users/%d", createdUser.User.ID))
+
+	s.Require().NoError(err)
+	s.Require().Equal(http.StatusOK, resp.StatusCode())
+	s.Equal(createdUser.User.ID, updatedUser.User.ID)
+	s.Equal("Framework", updatedUser.User.Name)
+	s.Equal("https://goravel.dev/avatar.png", updatedUser.User.Avatar)
+
+	// Get the User
+	var user struct {
+		User models.User
+	}
+	resp, err = s.http.R().SetResult(&user).Get(fmt.Sprintf("users/%d", createdUser.User.ID))
+
+	s.Require().NoError(err)
+	s.Require().Equal(http.StatusOK, resp.StatusCode())
+	s.True(user.User.ID > 0)
+	s.Equal("Framework", user.User.Name)
+	s.Equal("https://goravel.dev/avatar.png", user.User.Avatar)
+
+	// Delete the User
+	resp, err = s.http.R().Delete(fmt.Sprintf("users/%d", createdUser.User.ID))
+
+	s.Require().NoError(err)
+	s.Require().Equal(http.StatusOK, resp.StatusCode())
+	s.Equal("{\"rows_affected\":1}", resp.String())
+
+	// Get Users
+	resp, err = s.http.R().Get("users")
+
+	s.Require().NoError(err)
+	s.Require().Equal(http.StatusOK, resp.StatusCode())
+	s.Equal("{\"users\":[]}", resp.String())
+}
+
+func (s *RouteTestSuite) TestValidationJson() {
+	payload := strings.NewReader(`{
+		"name": "Goravel",
+		"date": "2024-07-08 18:33:32"
+	}`)
+
+	resp, err := s.http.R().SetBody(payload).Post("/validation/json")
+
+	s.NoError(err)
+	s.Equal(http.StatusOK, resp.StatusCode())
+	s.Equal("{\"date\":\"2024-07-08 18:33:32\",\"name\":\"Goravel\"}", resp.String())
+}
+
+func (s *RouteTestSuite) TestValidationRequest() {
+	payload := strings.NewReader(`{
+		"name": "Goravel",
+		"date": "2024-07-08 18:33:32",
+		"tags": ["tag1", "tag2"],
+		"scores": [1, 2]
+	}`)
+
+	resp, err := s.http.R().SetBody(payload).Post("/validation/request")
+
+	s.NoError(err)
+	s.Equal(http.StatusOK, resp.StatusCode())
+	s.Equal("{\"date\":\"2024-07-08 18:33:32\",\"name\":\"Goravel\",\"scores\":[1,2],\"tags\":[\"tag1\",\"tag2\"]}", resp.String())
 }
