4307c9c2586d6b0ba58d0d2ff74a09b997e2a71c
kkumar-gcc <krishkumar9352@gmail.com>
2024-11-09 11:25:41 +0530

    add http tests

--- routes/api.go
+++ routes/api.go
@@ -11,4 +11,14 @@ func Api() {
 	authController := controllers.NewAuthController()
 	facades.Route().Post("auth/login", authController.Login)
 	facades.Route().Middleware(middleware.Auth()).Get("auth/info", authController.Info)
+
+	testController := controllers.NewTestController()
+	facades.Route().Get("test/index", testController.Index)
+	facades.Route().Get("test/show/{id}", testController.Show)
+	facades.Route().Post("test/create", testController.Create)
+	facades.Route().Put("test/update/{id}", testController.Update)
+	facades.Route().Delete("test/delete/{id}", testController.Delete)
+	facades.Route().Get("test/custom-header", testController.CustomHeader)
+	facades.Route().Get("test/empty-response", testController.EmptyResponse)
+	facades.Route().Middleware(middleware.Auth()).Post("test/authorization", testController.Index)
 }
