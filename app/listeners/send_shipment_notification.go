7be6d092688f75e5e4d691792ea697d3c007977e
Bowen <hwbrzzl@gmail.com>
2024-11-20 15:49:34 +0800

    fix: [#523] [Bug] The same listener can not be registered by multiple events, framework will threw an error: job signature duplicate

---
+++ app/listeners/send_shipment_notification.go
@@ -0,0 +1,35 @@
+package listeners
+
+import (
+	"github.com/goravel/framework/contracts/event"
+	"github.com/spf13/cast"
+)
+
+var TestResultOfSendShipmentNotification []string
+
+type SendShipmentNotification struct {
+}
+
+func NewSendShipmentNotification() *SendShipmentNotification {
+	return &SendShipmentNotification{}
+}
+
+func (receiver *SendShipmentNotification) Signature() string {
+	return "send_shipment_notification"
+}
+
+func (receiver *SendShipmentNotification) Queue(args ...any) event.Queue {
+	return event.Queue{
+		Enable:     false,
+		Connection: "",
+		Queue:      "",
+	}
+}
+
+func (receiver *SendShipmentNotification) Handle(args ...any) error {
+	if len(args) > 0 {
+		TestResultOfSendShipmentNotification = append(TestResultOfSendShipmentNotification, cast.ToString(args[0]))
+	}
+
+	return nil
+}
---
+++ tests/feature/event_test.go
@@ -0,0 +1,27 @@
+package feature
+
+import (
+	"testing"
+
+	"github.com/goravel/framework/contracts/event"
+	"github.com/goravel/framework/facades"
+	"github.com/stretchr/testify/assert"
+
+	"goravel/app/events"
+	"goravel/app/listeners"
+)
+
+func TestEvent(t *testing.T) {
+	assert.NoError(t, facades.Event().Job(&events.OrderShipped{}, []event.Arg{
+		{Type: "string", Value: "I'm OrderShipped"},
+	}).Dispatch())
+
+	assert.NoError(t, facades.Event().Job(&events.OrderCanceled{}, []event.Arg{
+		{Type: "string", Value: "I'm OrderCanceled"},
+	}).Dispatch())
+
+	assert.ElementsMatch(t, []string{
+		"I'm OrderShipped",
+		"I'm OrderCanceled",
+	}, listeners.TestResultOfSendShipmentNotification)
+}
