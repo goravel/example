db8c07931c38b636b2b33d4d6b6177d906a1b143
Bowen <hwbrzzl@gmail.com>
2025-03-23 13:18:13 +0800

    add extend auth example

--- app/providers/auth_service_provider.go
+++ app/providers/auth_service_provider.go
@@ -1,7 +1,10 @@
 package providers
 
 import (
+	framewrokauth "github.com/goravel/framework/auth"
+	contractsauth "github.com/goravel/framework/contracts/auth"
 	"github.com/goravel/framework/contracts/foundation"
+	"github.com/goravel/framework/facades"
 )
 
 type AuthServiceProvider struct {
@@ -12,5 +15,7 @@ func (receiver *AuthServiceProvider) Register(app foundation.Application) {
 }
 
 func (receiver *AuthServiceProvider) Boot(app foundation.Application) {
-
+	facades.Auth().Extend("session", func(name string, auth contractsauth.Auth, userProvider contractsauth.UserProvider) (contractsauth.GuardDriver, error) {
+		return framewrokauth.NewJwtGuard(app.Request(), name, facades.Cache(), facades.Config(), userProvider)
+	})
 }
