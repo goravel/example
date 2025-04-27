d48669b6e6b115ee9acc3a19e1af2b47b4dd2232
alexander6463 <ad@uktlab.ru>
2025-04-27 21:34:55 +0300

    docker db issue

--- tests/feature/main_test.go
+++ tests/feature/main_test.go
@@ -1,18 +1,26 @@
 package feature
 
 import (
-	"os"
-	"testing"
-
+	"fmt"
 	"github.com/goravel/framework/facades"
 	"github.com/goravel/framework/support/file"
+	"github.com/stretchr/testify/suite"
+	"goravel/tests"
+	"testing"
+	"time"
 )
 
-func TestMain(m *testing.M) {
-	database, err := facades.Testing().Docker().Database()
+type TransactionsSuite struct {
+	suite.Suite
+	tests.TestCase
+}
+
+func TestTransactionsSuite(t *testing.T) {
+	database, err := facades.Testing().Docker().Database("postgres")
 	if err != nil {
 		panic(err)
 	}
+	fmt.Println(database)
 
 	if err := database.Build(); err != nil {
 		panic(err)
@@ -21,15 +29,31 @@ func TestMain(m *testing.M) {
 	if err := database.Migrate(); err != nil {
 		panic(err)
 	}
-
-	exit := m.Run()
-
+	fmt.Println(database.Config().Host)
+	fmt.Println(database.Config().Username)
+	fmt.Println(database.Config().Password)
+	fmt.Println(database.Config().Database)
+	fmt.Println(database.Config().Port)
+	time.Sleep(120 * time.Second)
+	suite.Run(t, new(TransactionsSuite))
 	if err := file.Remove("storage"); err != nil {
 		panic(err)
 	}
 	if err := database.Shutdown(); err != nil {
 		panic(err)
 	}
+}
 
-	os.Exit(exit)
+// SetupTest will run before each test in the suite.
+func (s *TransactionsSuite) SetupTest() {
+	fmt.Println("SetupTest")
+}
+
+// TearDownTest will run after each test in the suite.
+func (s *TransactionsSuite) TearDownTest() {
+	s.RefreshDatabase()
+}
+
+func (s *TransactionsSuite) Testdb() {
+	fmt.Println("Testttt")
 }
