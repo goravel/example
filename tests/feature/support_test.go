626625c127b73a4d486c4ed4bde94217b77970e9
Bowen <hwbrzzl@gmail.com>
2026-01-01 18:44:00 +0800

    optimize

--- tests/feature/support_test.go
+++ tests/feature/support_test.go
@@ -1,12 +1,9 @@
 package feature
 
 import (
-	"fmt"
-	"os"
 	"strings"
 	"testing"
 
-	"github.com/goravel/framework/support"
 	"github.com/goravel/framework/support/path"
 	"github.com/stretchr/testify/assert"
 )
@@ -19,17 +16,4 @@ func TestPathResource(t *testing.T) {
 	resourcePath = path.Resource("test.txt")
 	assert.True(t, strings.HasPrefix(resourcePath, "/"))
 	assert.True(t, strings.HasSuffix(resourcePath, "/resources/test.txt"))
-}
-
-func TestChangeChdir(t *testing.T) {
-	info, err := os.Stat("go.mod")
-	assert.Nil(t, info)
-	assert.Error(t, err)
-
-	fmt.Println(support.RelativePath)
-	t.Chdir(support.RelativePath)
-
-	info, err = os.Stat("go.mod")
-	assert.NotNil(t, info)
-	assert.Nil(t, err)
 }
