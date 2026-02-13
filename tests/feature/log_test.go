fe03b947d2a649f26e71c4db83f9329fa31c6c45
Bowen <hwbrzzl@gmail.com>
2026-02-13 22:53:51 +0800

    add log test

--- tests/feature/log_test.go
+++ tests/feature/log_test.go
@@ -16,9 +16,22 @@ func TestLog(t *testing.T) {
 	carbon.SetTestNow(carbon.Parse(dateTimeMilli))
 	defer carbon.ClearTestNow()
 
-	dailyLogPath := path.Storage("logs", "goravel-"+now.ToDateString()+".log")
-
 	facades.Log().Info("This is an info log")
+
 	assert.True(t, file.Contains(path.Storage("logs", "goravel.log"), `{"environment":"local","level":"info","message":"This is an info log","time":"`+dateTimeMilli+`"}`))
+
+	dailyLogPath := path.Storage("logs", "goravel-"+now.ToDateString()+".log")
 	assert.True(t, file.Contains(dailyLogPath, "["+dateTimeMilli+"] local.info: This is an info log"))
+
+	// The next day's log file can be created correctly
+	now = carbon.Now().AddDay()
+	dateTimeMilli = now.ToDateTimeMilliString()
+	carbon.SetTestNow(carbon.Parse(dateTimeMilli))
+
+	facades.Log().Error("This is an error log")
+
+	assert.True(t, file.Contains(path.Storage("logs", "goravel.log"), `{"environment":"local","level":"error","message":"This is an error log","time":"`+dateTimeMilli+`","trace":{`))
+
+	newDailyLogPath := path.Storage("logs", "goravel-"+now.ToDateString()+".log")
+	assert.True(t, file.Contains(newDailyLogPath, "["+dateTimeMilli+"] local.error: This is an error log"))
 }
