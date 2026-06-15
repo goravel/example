774c16e0501a22436e6e02f2c170efa8fcdafeec
Bowen <hwbrzzl@gmail.com>
2026-06-15 15:47:44 +0800

    optimize

--- tests/feature/db_drivers_test.go
+++ tests/feature/db_drivers_test.go
@@ -3,7 +3,6 @@ package feature
 import (
 	"testing"
 
-	"github.com/goravel/framework/support/env"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/suite"
 
@@ -11,10 +10,6 @@ import (
 )
 
 func TestDBDrivers(t *testing.T) {
-	if env.IsWindows() {
-		t.Skip("Skipping database driver tests on Windows due to potential issues with Docker.")
-	}
-
 	connections := []string{"postgres", "mysql", "sqlserver"}
 
 	for _, connection := range connections {
